184068ae32a0d43b11f99a6b4016e0039450e016
dennis <isacsson_89@hotmail.com>
2024-09-30 09:32:42 +0200

    arduinoCode3

--- arduinoMagic.ino
+++ arduinoMagic.ino
@@ -1,44 +1,185 @@
-/*   WiFiClient client = server.available();
+#include <WiFiS3.h>
+#include <ArduinoHttpClient.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#include "settings.h"
 
-  if (client) {
-    Serial.println("Ny klient ansluten!");
-    String request = client.readStringUntil('\r');
-    Serial.println(request);
+#define DHTPIN 7
+#define DHTTYPE DHT11
 
-    if (request.indexOf("/ON") != -1) {
-      ledStatus = "ON";
-      Serial.println("TÄND LAMPAN");
-      digitalWrite(led,HIGH);
-    } else if (request.indexOf("/OFF") != -1) {
-      ledStatus = "OFF";
-      Serial.println("SLÄCK LAMPAN");
-      digitalWrite(led,LOW); 
-    }
+DHT dht(DHTPIN, DHTTYPE);
 
-    client.println("HTTP/1.1 200 OK");
-    client.println("Content-type:text/html");
-    client.println();
-    client.println("<html><body>");
-    if (ledStatus == "OFF") {
-      client.println("<h1>LED IS TURNED OFF!</h1>");
-      client.print("<a href='http://");
-;      client.print(ip);
-      client.println("/ON'>TURN ON</a>");
-    } else {
-      client.println("<h1>LED IS TURNED ON!</h1>");
-       client.print("<a href='http://");
-      client.print(ip);
-      client.println("/OFF'>TURN OFF</a>");
-    }
-    
-    client.println("</body></html>");
-    client.println();
+int sendDataLedOK = 2;
+int sendDataLedNOTOK = 4;
+int buttonPin = 13;
+int buttonState = 1;
 
-    delay(2000);
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASSWORD;
 
-    client.stop();
-    Serial.println("Klient har kopplat ifrån!");
+char serverAdress[] = "192.168.0.215";
+int port = 3000;
 
+WiFiClient wifi;
+HttpClient client = HttpClient(wifi, serverAdress, port);
+
+WiFiServer server(80);
+
+IPAddress ip;
+unsigned long previousTempMillis = 0;
+unsigned long previousLiveMillis = 0;
+const long tempInterval = 1800000; 
+const long liveInterval = 30000;   
+
+void setup() {
+  pinMode(sendDataLedOK, OUTPUT);
+  pinMode(sendDataLedNOTOK, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
+  dht.begin();
+
+  Serial.println("Ansluter till WIFI...");
+
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+    Serial.println(".");
+    delay(1000);
+  }
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  ip = WiFi.localIP();
+  Serial.print("IP Adress: ");
+  Serial.println(ip);
+  //server.begin();
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+
+  unsigned long currentMillis = millis();
+  
+  if (currentMillis - previousTempMillis >= tempInterval) {
+    previousTempMillis = currentMillis;
+    sendTempToBackend();
+  }
+  
+  if (currentMillis - previousLiveMillis >= liveInterval) {
+    previousLiveMillis = currentMillis;
+    sendLiveTempToBackend();
+    Serial.print(currentMillis / 1000);
+    Serial.println(" sekunder");
+  }
+  
+  if (buttonState == LOW) {
+    Serial.println("Tryck på knapp!");
+    delay(50);
+    sendTempToBackend();
+  }
+}
+
+void sendTempToBackend() {
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("Failed to read from DHT sensor!");
+    digitalWrite(sendDataLedNOTOK, HIGH);
+    delay(5000);
+    digitalWrite(sendDataLedNOTOK, LOW);
+    return;
   }
 
-  delay(500); */
+  String postData = "{\"temp\": \"" + String(temperature) + "\", \"humidity\": \"" + String(humidity) + "\"}";
+  Serial.println("Skickar data till databasen!");
+  client.beginRequest();
+  client.post("/temp");
+
+  client.sendHeader("Content-Type", "application/json");
+  client.sendHeader("Content-length", postData.length());
+
+  client.beginBody();
+  client.print(postData);
+  client.endRequest();
+
+  int statusCode = client.responseStatusCode();
+  String response = client.responseBody();
+
+  Serial.print("Status code: ");
+  Serial.println(statusCode);
+  Serial.print("Response: ");
+  Serial.println(response);
+  Serial.print("Temperature = ");
+  Serial.println(temperature);
+  Serial.print("Humidity = ");
+  Serial.println(humidity);
+
+  if (statusCode == 200) {
+    digitalWrite(sendDataLedNOTOK, LOW);
+    digitalWrite(sendDataLedOK, HIGH);
+    delay(300);
+    digitalWrite(sendDataLedOK, LOW);
+    delay(300);
+    digitalWrite(sendDataLedOK, HIGH);
+    delay(300);
+    digitalWrite(sendDataLedOK, LOW);
+    delay(300);
+    digitalWrite(sendDataLedOK, HIGH);
+    delay(300);
+    digitalWrite(sendDataLedOK, LOW);
+    delay(300);
+    digitalWrite(sendDataLedOK, HIGH);
+    delay(3000);
+    digitalWrite(sendDataLedOK, LOW);
+  } else {
+    Serial.print("Error kunde inte skicka data!");
+    digitalWrite(sendDataLedNOTOK, HIGH);
+  }
+}
+
+void sendLiveTempToBackend() {
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("Kunde inte läsa från DHT sensor!");
+    digitalWrite(sendDataLedNOTOK, HIGH);
+    delay(5000);
+    digitalWrite(sendDataLedNOTOK, LOW);
+    return;
+  }
+
+  String postData = "{\"temp\": \"" + String(temperature) + "\", \"humidity\": \"" + String(humidity) + "\"}";
+  Serial.println("Skickar LiveTemp");
+  client.beginRequest();
+  client.post("/live");
+
+  client.sendHeader("Content-Type", "application/json");
+  client.sendHeader("Content-length", postData.length());
+
+  client.beginBody();
+  client.print(postData);
+  client.endRequest();
+
+  int statusCode = client.responseStatusCode();
+  String response = client.responseBody();
+
+  Serial.print("Status code: ");
+  Serial.println(statusCode);
+  Serial.print("Response: ");
+  Serial.println(response);
+  Serial.print("Temperature = ");
+  Serial.println(temperature);
+  Serial.print("Humidity = ");
+  Serial.println(humidity);
+
+  if (statusCode == 200) {
+    digitalWrite(sendDataLedNOTOK, LOW);
+    digitalWrite(sendDataLedOK, HIGH);
+    delay(1000);
+    digitalWrite(sendDataLedOK, LOW);
+  } else {
+    Serial.print("Error kunde inte skicka data!");
+    digitalWrite(sendDataLedNOTOK, HIGH);
+  }
+}
